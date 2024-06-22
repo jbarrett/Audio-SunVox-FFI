fe0ff06c505cf0fa06dacfad72fc13858a463f92
John Barrett <john@jbrt.org>
2024-06-22 18:48:04 +0100

    Get README from main module

--- lib/Audio/SunVox/FFI.pm
+++ lib/Audio/SunVox/FFI.pm
@@ -1,6 +1,50 @@
 use strict;
 use warnings;
 package Audio::SunVox::FFI;
+
+# ABSTRACT: Bindings for the SunVox library - a modular synthesizer and sequencer
+
+=encoding UTF-8
+
+=head1 SYNOPSIS
+
+    use Audio::SunVox::FFI ':all';
+    
+    # Initialise
+    my $audiodriver = 'asio';
+    my $audiodevice = 1;
+    my $buffer      = 256;
+    my $frequency   = 48_000;
+    my $channels    = 2;
+    sv_init( "audiodriver=$audiodriver|audiodevice=$audiodevice|buffer=$buffer", $frequency, $channels );
+    
+    # Open a "slot" - an instance of SunVox
+    my $slot = 0;
+    sv_open_slot( $slot );
+    
+    # Create a "generator" oscillator
+    sv_lock_slot( $slot );
+    my $generator = sv_new_module( $slot, "Generator", "foo name" );
+    sv_connect_module( $slot, $generator, 0 ); # 0 is Output
+    sv_unlock_slot( $slot );
+    
+    # Send an event to the generator
+    sv_set_event_t( $slot, 1, 0 ); # Process events in real time
+    sv_set_module_ctl_value( $slot, $generator, 7, 0, 2 ); # Disable sustain
+    sv_set_module_ctl_value( $slot, $generator, 4, 200, 2 ); # Set release value
+    sv_send_event( $slot, 0, 50, 127, $generator + 1 ); # Send a note on event
+    # ^ Why $generator + 1? Dunno yet...
+    sleep(1);
+    
+    # Save the patch
+    sv_save( $slot, 'awesome_patch.sunvox' );
+    
+    # Clean up
+    sv_close_slot( 0 );
+    sv_deinit;
+
+=cut
+
 use base qw/ Exporter /;
 
 use FFI::Platypus 2.00;
@@ -8,8 +52,6 @@ use FFI::CheckLib 0.25 qw/ find_lib_or_die /;
 use Carp qw/ croak carp /;
 
 our $VERSION = '0.00';
-
-# ABSTRACT: Bindings for the SunVox library - a modular synthesizer and sequencer
 
 our $lofi = 0;
 my $ffi;
@@ -187,45 +229,6 @@ our %EXPORT_TAGS     = ( all => \@EXPORT_OK, constants => \@export_constants, bi
 
 
 1;
-
-__END__
-
-=head1 SYNOPSIS
-
-    use Audio::SunVox::FFI ':all';
-    
-    # Initialise
-    my $audiodriver = 'asio';
-    my $audiodevice = 1;
-    my $buffer      = 256;
-    my $frequency   = 48_000;
-    my $channels    = 2;
-    sv_init( "audiodriver=$audiodriver|audiodevice=$audiodevice|buffer=$buffer", $frequency, $channels );
-    
-    # Open a "slot" - an instance of SunVox
-    my $slot = 0;
-    sv_open_slot( $slot );
-    
-    # Create a "generator" oscillator
-    sv_lock_slot( $slot );
-    my $generator = sv_new_module( $slot, "Generator", "foo name" );
-    sv_connect_module( $slot, $generator, 0 ); # 0 is Output
-    sv_unlock_slot( $slot );
-    
-    # Send an event to the generator
-    sv_set_event_t( $slot, 1, 0 ); # Process events in real time
-    sv_set_module_ctl_value( $slot, $generator, 7, 0, 2 ); # Disable sustain
-    sv_set_module_ctl_value( $slot, $generator, 4, 200, 2 ); # Set release value
-    sv_send_event( $slot, 0, 50, 127, $generator + 1 ); # Send a note on event
-    # ^ Why $generator + 1? Dunno yet...
-    sleep(1);
-    
-    # Save the patch
-    sv_save( $slot, 'awesome_patch.sunvox' );
-    
-    # Clean up
-    sv_close_slot( 0 );
-    sv_deinit;
 
 =head1 DESCRIPTION
 
